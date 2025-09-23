f12cfcf699f400ab6de34b1cf91e6e7b840fd0a7
Priyadharshini M <priyadharshini02843@gmail.com>
2025-09-23 21:37:36 +0530

    Created

---
+++ src/main/BFS.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+void bfs(int start, const vector<vector<int>>& adj, vector<bool>& visited) {
+    queue<int> q;
+    q.push(start);
+    visited[start] = true;
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        cout << node << " ";
+
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                q.push(neighbor);
+                visited[neighbor] = true;
+            }
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    cout << "Enter number of nodes: ";
+    cin >> n;
+    cout << "Enter number of edges: ";
+    cin >> m;
+
+    vector<vector<int>> adj(n);
+    vector<bool> visited(n, false);
+
+    cout << "Enter edges (u v) where u and v are node indices (0 to " << n-1 << "):\n";
+    for (int i = 0; i < m; ++i) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    int start;
+    cout << "Enter starting node for BFS: ";
+    cin >> start;
+
+    cout << "BFS Traversal: ";
+    bfs(start, adj, visited);
+
+    return 0;
+}
+
